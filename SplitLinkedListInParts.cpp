da2e6877e750aedaa594e7bcb559508ccf0e1f30
shimon0505004 <shimon0505004@gmail.com>
2017-12-07 22:50:19 -0800

    Solution for Spliting Linked List in Parts (https://leetcode.com/problems/split-linked-list-in-parts/)

---
+++ SplitLinkedListInParts.cpp
@@ -0,0 +1,49 @@
+/*
+	https://leetcode.com/problems/split-linked-list-in-parts/
+	shimon0505004
+*/
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<ListNode*> splitListToParts(ListNode* root, int k) {
+        vector<ListNode*> result (k,nullptr);
+        vector<int> maxSize(k,0);
+        
+        int listLength = 0;
+        //Run first pass to count number of elements in the list.
+        for(ListNode* cur=root;cur!=nullptr;cur = cur->next, ++listLength)
+           maxSize[listLength%k]++;     
+        
+        int currentIndex = 0;
+        int currentSizeAtIndex = 0;
+        result[currentIndex] = root;
+        ListNode* prev=nullptr;
+
+        for(ListNode* cur=root;cur!=nullptr;cur = cur->next)
+        {
+            if(currentSizeAtIndex == maxSize[currentIndex])
+            {                    
+                currentIndex++;
+                currentSizeAtIndex = 0;
+                
+                result[currentIndex]= cur;
+                
+                if(prev != nullptr)
+                    prev->next= nullptr;
+            }
+            currentSizeAtIndex++;
+            prev = cur;
+        }
+        
+        return result;
+    }
+};
