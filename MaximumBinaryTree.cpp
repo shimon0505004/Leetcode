adc75a9c2cb97ed51e890f7588147b92941a8e02
shimon0505004 <shimon0505004@gmail.com>
2017-12-07 22:17:08 -0800

    Solution of "Maximum Binary Tree" (https://leetcode.com/problems/maximum-binary-tree/)

---
+++ MaximumBinaryTree.cpp
@@ -0,0 +1,38 @@
+/*
+	https://leetcode.com/submissions/detail/129855645/
+	shimon0505004
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
+        if(nums.size()==0)
+            return nullptr;
+        else if(nums.size()==1)
+            return new TreeNode(nums.at(0));
+        else
+        {
+            std::vector<int>::iterator max_element_iterator = std::max_element(nums.begin(), nums.end());
+            TreeNode* root = new TreeNode((*max_element_iterator));
+            
+            vector<int> rightHalf(max_element_iterator+1,nums.end());
+            nums.erase(max_element_iterator,nums.end());
+            
+            
+            
+            root->left = constructMaximumBinaryTree(nums);
+            root->right = constructMaximumBinaryTree(rightHalf);
+            
+            return root;
+        }
+    }
+};
