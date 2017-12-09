dd390d998722317da2b2652b0dc5868dde0cc44d
shimon0505004 <shimon0505004@gmail.com>
2017-12-09 09:05:30 -0800

    Solution for "Set Mismatch" (https://leetcode.com/problems/set-mismatch/)

---
+++ SetMismatch.cpp
@@ -0,0 +1,28 @@
+/*
+	https://leetcode.com/problems/set-mismatch/
+	shimon0505004
+*/
+
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        std::unordered_set<int> keys;
+        vector<int> result;
+        for(const int& num: nums){
+            if(keys.count(num) == 0)
+                keys.insert(num);
+            else
+                result.push_back(num);
+        }
+        
+        for(int missingNumber=1; missingNumber <= nums.size() ; missingNumber++){
+            if(keys.count(missingNumber) == 0)
+            {
+                result.push_back(missingNumber);
+                break;
+            }
+        }
+        
+        return result;
+    }
+};
