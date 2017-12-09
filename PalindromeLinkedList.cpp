a7b00eeb0defaad025d4de1f654d705f9b100652
shimon0505004 <shimon0505004@gmail.com>
2017-12-09 09:03:09 -0800

    Solution for "Palindromic Linked List" (https://leetcode.com/problems/palindrome-linked-list/)

---
+++ PalindromeLinkedList.cpp
@@ -0,0 +1,37 @@
+/*
+	https://leetcode.com/problems/palindrome-linked-list/
+	shimon0505004
+*/
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        std::stack<int> palindromeStack;
+        for(auto node = head
+            ;node != nullptr
+            ;node = node->next){
+            palindromeStack.push(node->val);
+        }
+        
+        for(auto node = head
+            ;node != nullptr
+            ;node = node->next){
+            if(node->val != palindromeStack.top())
+                return false;
+            
+            palindromeStack.pop();
+        }
+        
+        return true;
+        
+    }
+};
