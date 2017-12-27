375575b1a4cb317ddb8b0c6571e8a9b37bf351ee
shimon0505004 <shimon0505004@gmail.com>
2017-12-27 13:23:27 -0800

    073: Set Matrix Zero : Using constant spaces.

--- 073_SetMatrixZero.cpp
+++ 073_SetMatrixZero.cpp
@@ -5,7 +5,9 @@
 
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& matrix) {
+    
+    //O(m+n) space complexity.
+    void setZeroes_UsingUnorderedSet(vector<vector<int>>& matrix) {
         
         //Pass 1. Set first row and first columns to zero for appropriate places.
         std::unordered_set<int> rowsToBeCleared;
@@ -40,5 +42,67 @@ public:
             }            
         }
 
+    }
+    
+    //O(1) space complexity.
+    void setZeroes_UsingConstantSpace(vector<vector<int>>& matrix) {
+        
+        //Pass 1. Set first row and first columns to zero for appropriate places.
+
+        bool setFirstRowToZero = false;
+        bool setFirstColToZero = false;
+
+        
+        for(int row=0;row<matrix.size();row++)
+        {
+            for(int column=0;column<matrix.at(row).size();column++)
+            {
+                if(matrix[row][column]==0)
+                {
+                    if(row==0)
+                        setFirstRowToZero = true;
+                    
+                    if(column==0)
+                        setFirstColToZero = true;
+                    
+                    matrix[row][0] = 0;
+                    matrix[0][column] = 0;                    
+                }
+            }            
+        }
+        
+        //Pass 2. Edit the complete matrix.
+        for(int row=1;row<matrix.size();row++)
+        {
+            for(int column=1;column<matrix.at(row).size();column++)
+            {
+                if(matrix[0][column]==0)
+                    matrix[row][column] = 0;
+            
+                if(matrix[row][0]==0)
+                    matrix[row][column] = 0;
+            }
+        }
+        
+        if(setFirstRowToZero)
+        {
+            for(int column=0;column<matrix.at(0).size();column++)
+            {
+                matrix[0][column] = 0;
+            }
+        }
+
+        if(setFirstColToZero)
+        {
+            for(int row=0; row<matrix.size(); row++)
+            {
+                matrix[row][0] = 0;
+            }
+        }
+
+    }
+
+    void setZeroes(vector<vector<int>>& matrix) {
+        setZeroes_UsingConstantSpace(matrix);
     }
 };
